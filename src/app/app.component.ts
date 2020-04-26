0ede42c6019fd20686036fee756545ee8f340d57
Ishani Sojitra <sojitraishani@yahoo.com>
2020-04-26 17:28:27 +0530

    Third Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'print-func';
+export class AppComponent implements OnInit{
+  isEditForm : boolean = true;
+  todayDat : Date;
+  billForm : FormGroup;
 
+  constructor(private fb: FormBuilder){
+
+  }
+
+  ngOnInit(){
+    this.billForm = this.fb.group({
+      invoiceNo : "",
+      todayDate : "",
+      customerName : ""
+    });
+    this.todayDat = new Date();
+    var newDate = this.todayDat.getDate() + '/' + (this.todayDat.getMonth()+1) + '/' + this.todayDat.getFullYear();
+    this.billForm.controls.todayDate.setValue(newDate);
+  }
   onSubmit(){
     window.print();
+  }
+
+  backPage(){
+    this.isEditForm = true;
+  }
+
+  previewPage(){
+    this.isEditForm = false;
   }
 }

644e2c76012d04ed32be6be9289d4b36471fde6c
Ishani Sojitra <sojitraishani@yahoo.com>
2020-05-02 16:09:27 +0530

    more functionalities added

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataServiceService } from '../data-service.service';
 
 @Component({
   selector: 'app-main-page',
@@ -8,36 +10,70 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class MainPageComponent implements OnInit {
 
-  isEditForm : boolean = true;
-  todayDat : Date;
-  billForm : FormGroup;
+  isEditForm: boolean = true;
+  todayDat: Date;
+  billForm: FormGroup;
+  dataList = [];
+  data = [];
 
-  constructor(private fb: FormBuilder){
+  constructor(private fb: FormBuilder, private router: Router, private service: DataServiceService) {
 
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.billForm = this.fb.group({
-      invoiceNo : "",
-      todayDate : "",
-      goodsDesc : "",
-      hsnCode : "",
-      quantity : 0,
-      rate : 0,
-      gst : 0,
-      consignee : "",
-      custGST : "",
-      vehicleNo : "",
-      lrNo : ""
+      invoiceNo: "",
+      todayDate: "",
+      goodsDesc: "",
+      hsnCode: "",
+      quantity: 0,
+      rate: 0,
+      gst: 0,
+      consignee: "",
+      custGST: "",
+      vehicleNo: "",
+      lrNo: ""
     });
     this.todayDat = new Date();
-    var newDate = this.todayDat.getDate() + '/' + (this.todayDat.getMonth()+1) + '/' + this.todayDat.getFullYear();
+    var newDate = this.todayDat.getDate() + '/' + (this.todayDat.getMonth() + 1) + '/' + this.todayDat.getFullYear();
     this.billForm.controls.todayDate.setValue(newDate);
-  }
-  
 
-  backPage(){
-    this.isEditForm = true;
+    this.service.currentMessage.subscribe(data => this.data = data);
+    if (this.data != null) {
+      this.billForm.setValue({
+        invoiceNo: this.data[0].invoiceNo,
+        todayDate: this.data[0].todayDate,
+        goodsDesc: this.data[0].goodsDesc,
+        hsnCode: this.data[0].hsnCode,
+        quantity: this.data[0].quantity,
+        rate: this.data[0].rate,
+        gst: this.data[0].gst,
+        consignee: this.data[0].consignee,
+        custGST: this.data[0].custGST,
+        vehicleNo: this.data[0].vehicleNo,
+        lrNo: this.data[0].lrNo
+      })
+    }
+  }
+
+
+  previewPage() {
+    this.dataList.push({
+      'invoiceNo': this.billForm.get('invoiceNo').value,
+      'todayDate': this.billForm.get('todayDate').value,
+      'goodsDesc': this.billForm.get('goodsDesc').value,
+      'hsnCode': this.billForm.get('hsnCode').value,
+      'quantity': this.billForm.get('quantity').value,
+      'rate': this.billForm.get('rate').value,
+      'gst': this.billForm.get('gst').value,
+      'consignee': this.billForm.get('consignee').value,
+      'custGST': this.billForm.get('custGST').value,
+      'vehicleNo': this.billForm.get('vehicleNo').value,
+      'lrNo': this.billForm.get('lrNo').value,
+    });
+    console.log(this.dataList);
+    this.service.changeMessage(this.dataList);
+    this.router.navigate(['/preview']);
   }
 
 
--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from '../data-service.service';
+import numToWords from 'num-to-words';
 
 @Component({
   selector: 'app-preview',
@@ -6,13 +8,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preview.component.css']
 })
 export class PreviewComponent implements OnInit {
-  sr =  12000;
-  constructor() { }
+  data;
+  invoiceNo;
+  todayDate;
+  goodsDesc;
+  hsnCode;
+  quantity;
+  rate;
+  gst;
+  consignee;
+  custGST;
+  vehicleNo;
+  lrNo;
+
+  totAmount;
+  halfgst;
+  halfgstAmt;
+  grandTot;
+  roundOff;
+
+  grandTotWord;
+  totgstWord;
+
+  constructor(private service: DataServiceService) { }
 
   ngOnInit() {
+    this.service.currentMessage.subscribe(data => this.data = data);
+    console.log(this.data);
+    this.invoiceNo = this.data[0].invoiceNo;
+    this.todayDate = this.data[0].todayDate;
+    this.goodsDesc = this.data[0].goodsDesc;
+    this.hsnCode = this.data[0].hsnCode;
+    this.quantity = this.data[0].quantity;
+    this.rate = this.data[0].rate;
+    this.gst = this.data[0].gst;
+    this.consignee = this.data[0].consignee;
+    this.custGST = this.data[0].custGST;
+    this.vehicleNo = this.data[0].vehicleNo;
+    this.lrNo = this.data[0].lrNo;
+
+    this.totAmount = this.rate * this.quantity;
+    this.halfgst = this.gst / 2;
+    this.halfgstAmt = this.totAmount * (this.halfgst / 100);
+    this.grandTot = this.totAmount + (this.halfgstAmt * 2);
+    this.roundOff = Math.round(this.grandTot) - this.grandTot;
+    this.grandTot = Math.round(this.grandTot);
+
+    this.grandTotWord = (numToWords(this.grandTot, { ands: true })).toUpperCase() + " ONLY";
+    var re = /\-/gi;
+    this.grandTotWord = this.grandTotWord.replace(re, " ");
+
+    var gstTotal = this.halfgstAmt * 2;
+    var gstTotalPs = (gstTotal.toFixed(2)).split('.');
+    var newgstPs = parseInt(gstTotalPs[1]);
+    var newgstPsWord;
+  
+    var andF = false;
+    if(newgstPs != 0){
+      newgstPsWord = " And " + numToWords(newgstPs) + " Paise";
+    }
+    else{
+      newgstPsWord = "";
+      andF = true;
+    }
+
+    this.totgstWord =  numToWords(gstTotal, { ands: andF }) + newgstPsWord + " Only";
+    var re = /\-/gi;
+    this.totgstWord = this.totgstWord.replace(re, " ");
   }
 
-  onSubmit(){
+  onSubmit() {
     window.print();
   }
 
